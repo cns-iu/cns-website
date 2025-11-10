cad165898b9ed73d810b42741039e41b676cc2b9
Bruce W. Herr II <bherr@iu.edu>
2025-11-10 15:40:58 -0500

    Added more data and config. WIP.

---
+++ content-migrator/src/queries/people.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE MACRO slugify(input_string) AS (
+    trim(both '-' from regexp_replace(regexp_replace(lower(replace(input_string, '&#246;', 'o')), '[^a-z0-9]+', '-', 'g'), '-+', '-', 'g'))
+);
+
+COPY (
+  SELECT
+    slugify(name) as slug,
+    replace(name, '&#246;', 'ö') as name,
+    replace(lname, '&#246;', 'ö') as lastName,
+    display_order as displayOrder,
+    'https://cns-iu.github.io/content-migrator/data/images/people/' || fileName as image,
+    office,
+    phone,
+    fax,
+    email,
+
+    jobDescription,
+    education,
+    background,
+    interests,
+
+    startDate,
+    endDate
+  FROM 
+    tblPeople
+      JOIN vwWebPeopleBase USING (peopleId)
+      LEFT JOIN tblCenterMembers USING (peopleId)
+  ORDER BY endDate DESC NULLS FIRST, display_order
+) TO 'data/indexes/people.json' (FORMAT json, ARRAY true);
