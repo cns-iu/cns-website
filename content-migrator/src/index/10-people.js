b6f2dcc2f6422fec990c3b4d925fcf10d7fff26b
Daniel Bolin <axbolin@iu.edu>
2026-04-03 14:34:30 -0400

    fix: :bug: Format dates in people roles

--- content-migrator/src/index/10-people.js
+++ content-migrator/src/index/10-people.js
@@ -1,11 +1,16 @@
 import { join } from 'path';
-import { BASE_URL, index, INDEXES, readIndex, writeMinifiedJSON } from './utils.js';
+import { BASE_URL, formatDate, index, INDEXES, readIndex, writeMinifiedJSON } from './utils.js';
 
 export function writePeopleIndex() {
   const people = readIndex('people');
   for (const person of people) {
     if (person.image && !person.image?.startsWith('http')) {
       person.image = `${BASE_URL}/people/${person.slug}/${person.image}`;
+    }
+
+    for (const role of person.roles ?? []) {
+      role.startDate = formatDate(role.startDate);
+      role.endDate = formatDate(role.endDate);
     }
   }
   writeMinifiedJSON(join(INDEXES, 'app-people.json'), people);
