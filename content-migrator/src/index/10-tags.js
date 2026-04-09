1e21591ec2c30db29f1aecea6e7e78010f23cf23
Daniel Bolin <axbolin@iu.edu>
2026-04-09 17:10:13 -0400

    feat: Write category and display tags

--- content-migrator/src/index/10-tags.js
+++ content-migrator/src/index/10-tags.js
@@ -1,4 +1,29 @@
-import { index } from './utils.js';
+import { readFileSync } from 'fs';
+import YAML from 'js-yaml';
+import { join } from 'path';
+import { index, INDEXES, readIndex, writeMinifiedJSON } from './utils.js';
+
+export function writeCategoryTagsIndex() {
+  const config = YAML.load(readFileSync('../admin/config.yml', 'utf-8'));
+  const tags = config.collections.map((c) => {
+    return {
+      slug: c.name,
+      name: c.label,
+      description: '',
+    };
+  });
+
+  writeMinifiedJSON(join(INDEXES, 'app-category-tags.json'), tags);
+}
+
+export function writeDisplayTagsIndex() {
+  const tags = readIndex('tags');
+  const categoryTags = readIndex('app-category-tags');
+  const displayTags = categoryTags.concat(tags).filter((tag) => !tag.hidden);
+  writeMinifiedJSON(join(INDEXES, 'app-display-tags.json'), displayTags);
+}
 
 index('tags/**/data.yaml', 'tags.json');
 index('tags/**/data.yaml', 'app-tags.json', true);
+writeCategoryTagsIndex();
+writeDisplayTagsIndex();
